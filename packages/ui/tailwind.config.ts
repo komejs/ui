70028cc3224d7d299bb6e33d74075b74d9748652
wangfeng <1272171893@qq.com>
2024-06-18 20:22:14 +0800

    修改架构

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import sharedConfig from "kome-tailwind";
+import sharedConfig from "../../tailwind.config";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
   content: ["./src/**/*.tsx"],
